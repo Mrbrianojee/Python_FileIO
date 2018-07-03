720eb549603833b987d900e12d482aad04b4bb94
Brianojee <brian@codeinstitute.net>
2018-07-03 17:56:33 +0000

    modified quiz to read absolute path

--- quiz.py
+++ quiz.py
@@ -1,4 +1,15 @@
-f = open("files/relative_data.txt", 'r')
+f = open("/home/ubuntu/workspace/files/relative_data.txt", 'r')
 lines =  f.read()
 f.close()
 print(lines)
+
+
+
+
+# brianojee:~/workspace/files (master) $ ls
+# relative_data.txt
+# brianojee:~/workspace/files (master) $ pwd
+# /home/ubuntu/workspace/files
+# brianojee:~/workspace/files (master) $ readlink -f relative_data.txt
+# /home/ubuntu/workspace/files/relative_data.txt
+# brianojee:~/workspace/files (master) $ 
