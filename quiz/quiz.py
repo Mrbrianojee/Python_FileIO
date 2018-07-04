f7a0d32914f33f5cd14b65e696ee9823f07edacf
Brianojee <brian@codeinstitute.net>
2018-07-04 13:24:11 +0000

    added ask_question()partial - question displaying only

--- quiz/quiz.py
+++ quiz/quiz.py
@@ -9,7 +9,7 @@ def game_loop():
     while True:
         option= show_menu()
         if option == 1:
-            print("You selected: Ask Questions")
+            ask_question()
         elif option == 2:
             add_question()
         elif option == 3:
@@ -20,7 +20,21 @@ def game_loop():
         print("")
     return option
 
-
+def ask_question():
+    questions = []
+    answers = []
+    
+    with open('questions.txt', 'r') as file:
+        lines =  file.read().splitlines()
+    
+    for i, text in enumerate(lines):
+        if i%2 == 0:
+            questions.append(text)
+        else:
+            answers.append(text)
+    
+    for question, answer in zip(questions, answers):
+        guess = raw_input(question + "> ")
 
 def add_question():
     print("")
